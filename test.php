e651c09a263bf6352c35b0e0aa7c3a43150fd74b
NicholasRFrintz <61771076+NicholasRFrintz@users.noreply.github.com>
2021-01-21 09:36:28 -0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<style>
+img {
+  float: left;
+  margin: 5px 5px 5px 5px;
+}
+table, td, th {
+            border: 1px solid black;
+            width: 300px;
+         }
+</style>
+<title>PHP Test</title>
+</head>
+<body>
+<h1 style="text-align:center">Simple HTML & CSS Page</h1>
+<p><img src="images/profilepic-adj2.jpg" width="200" height="300"> My name is Douglas Cheung and I am a student at CSM for 3 years at this point. I have what could only be described as an obsession with my own story series.</p>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br /> 
+<br /> 
+<p>I am attending this class because I desire knowhow in web development. So far, it's not as hard as I'm led to believe, and I hope that that's not the case.</p>
+<h2 style="text-align:center">Technologies for this class</h2>
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+</ul>
+<h2 style="text-align:center">Resources</h2>
+<ul>
+  <li><a href="https://www.w3schools.com/">W3Schools</a> - This website references all things HTML, CSS and JavaScript for building web pages</li>
+  <li><a href="https://stackoverflow.com/">StackOverflow</a> - This website is a Q&A site for anything related to code.</li>
+</ul>
+</body>
+</html>
