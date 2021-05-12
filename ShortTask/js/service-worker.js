8cea0140cc915efbe8b972c8ef732e2c110f5f6f
NicholasRFrintz <61771076+NicholasRFrintz@users.noreply.github.com>
2021-05-12 11:14:58 -0700

    Update service-worker.js

--- ShortTask/js/service-worker.js
+++ ShortTask/js/service-worker.js
@@ -1,8 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
-}
 document.getElementById("para1").innerHTML = "Let us start off the comparison with a short introduction.";
 document.getElementById("para2").innerHTML = "Hybrid Applications are any web applications built using HTML, CSS and JavaScript, which is then wrapped inside of a native app with help from some special container platforms. It is a shorthand combination of elements with both web and native development approaches.";
 document.getElementById("para3").innerHTML = "On this page, three such Hybrid Applications will be compared: PhoneGap, Flutter and React Native.";
