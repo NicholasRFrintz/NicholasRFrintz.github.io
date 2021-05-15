d568f034b23db0a912b6c43c2c6ba4f8a53070cb
NicholasRFrintz <61771076+NicholasRFrintz@users.noreply.github.com>
2021-05-15 14:45:16 -0700

    Update service-worker.js

--- ShortTask/js/service-worker.js
+++ ShortTask/js/service-worker.js
@@ -5,5 +5,5 @@ document.getElementById('t11').innerHtml = '<img src="' +images/phonegap.png+'"/
 document.getElementById("t12").innerHTML = "Developed by Adobe System in 2011, PhoneGap is a software development framework primarily used to develop mobile applications. It can produce apps for iOS, Android, BlackBerry and Windows Mobile OS. It only uses HTML, CSS, and Jscript for its languages. Today it is known as Apache Cordova";
 document.getElementById('t21').innerHtml = '<img src="' +images/flutter.png+'"/>';
 document.getElementById("t22").innerHTML = "Flutter is an open-source mobile SDK used to build native Android and iOS applications. It has been around since 2015, released in 2018 and uses C, C++ and Dart languages. An example of a Flutter app would be the Xianyu app created by the Alibaba team, used by over 50 million people to date";
-document.getElementById('t31').innerHtml = '<img src="' +images/react native.png+'"/>';
+document.getElementById('t31').innerHtml = '<img src="' +images/react%20native.png+'"/>';
 document.getElementById("t32").innerHTML = "An open-source mobile application framework created by Facebook and released in 2015. It utilizes JavaScript, Java, C++ and Python for its languages for developing Android, Android TV, iOS, macOS, tvOS, Web, Windows and UWP compatible apps";
